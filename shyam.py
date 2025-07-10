9632c7ae6da7d4f575bf5c5203fa3d8fa8c9828c
sawyaam03 <sawyaam321@gmail.com>
2025-07-10 21:52:20 +0545

    Add files via upload

---
+++ shyam.py
@@ -0,0 +1,16 @@
+# name = input("Enter your name:")
+# age = int(input("Enter your age:"))
+# print (f"""
+#        your name is {name} and your age is {age}.
+#        After 5 year your age will be {age +5}
+#         """)
+
+# if  statement
+age = int (input("Enter age:"))
+if age >= 18:
+    print("You can apply for citizenship card")
+    print("Please visit office with your birth certificate and parents")
+else:  # else means otherwise
+    print("You cannot apply for citizenship card")
+    print("Your current age is not enough to apply for citizenship card")
+ 
